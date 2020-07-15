288e7a0b2cdc8680c4d741c692dba7e54c22ad40
Aleksandr Sarkisov <quopiks@gmail.com>
2020-07-15 13:49:29 +0300

    some minnor changes

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import StarRating from './StarRating';
 import '../styles/Color.scss';
 
-const Color = ({
-    title,
-    color,
-    rating = 0,
-    onRemove = (f) => f,
-    onRate = (f) => f,
-}) => (
-    <section className="color">
-        <h1>{title}</h1>
-        <button onClick={onRemove}>X</button>
-        <div className="color" style={{backgroundColor: color}}></div>
-        <div>
-            <StarRating starsSelected={rating} onRate={onRate} />
-        </div>
-    </section>
-);
+class Color extends Component {
+    componentWillMount() {
+        this.style = {backgroundColor: '#CCC'};
+    }
+    shouldComponentUpdate(nextProps) {
+        const {rating} = this.props;
+        return rating !== nextProps.rating;
+    }
+    componentWillUpdate() {
+        this.style = null;
+    }
+    render() {
+        const {title, rating, color, onRate} = this.props;
+        return (
+            <section className="color" style={this.style}>
+                <h1 ref="title">{title}</h1>
+                <div className="color" style={{backgroundColor: color}}></div>
 
+                <StarRating starsSelected={rating} onRate={onRate} />
+            </section>
+        );
+    }
+}
 Color.propTypes = {
     title: PropTypes.string.isRequired,
     color: PropTypes.string.isRequired,
     rating: PropTypes.number,
     onRemove: PropTypes.func,
     onRate: PropTypes.func,
+};
+
+Color.defaultProps = {
+    rating: 0,
+    onRemove: (f) => f,
+    onRate: (f) => f,
 };
 
 export default Color;
